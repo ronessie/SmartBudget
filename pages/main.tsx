e6ffc243510820f3e5794449b25f27b2eb62ed8b
vorgnomov <sakolinskayaveronika@mail.ru>
2023-11-18 23:55:28 +0300

    update main page

--- pages/main.tsx
+++ pages/main.tsx
@@ -1,9 +1,14 @@
 import styles from '../styles/pages.module.css'
 
+
 export default function Page() {
     return(
         <div className={styles.page}>
             <div className={styles.pages}>
+                <div className={styles.conteiners}>
+                    <h1 className={styles.bigBlackText}>Hi, UserName</h1>
+                    <button className={styles.logOutButton}>Выйти</button>
+                </div>
                 <h1 className={styles.text}>Ваш счёт</h1>
                 <div className={styles.rectangle}><br/>
                     <h1 className={styles.whiteText}>Счёт 1</h1><br/>
