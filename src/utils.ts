d78eaf0ebb27eff80b2d30024e15d1e0943e3ec7
Yuri Ratkevich <yuri.ratkevich85360@gmail.com>
2024-02-16 05:33:01 +0300

    feat: add account to database from google auth

---
+++ src/utils.ts
@@ -0,0 +1,35 @@
+﻿import {ObjectId} from "bson";
+import IUser from "@/src/types/IUser";
+import IBankAccount from "@/src/types/IBankAccount";
+
+export function createUserObj(fio: string, email: string, password: string, bankAccount_id: ObjectId): IUser {
+    return {
+        _id: new ObjectId(),
+        fio: fio,
+        email: email,
+        password: password,
+        status: "Authorized",
+        currentBankAccount: bankAccount_id,
+        twoStepAuth: false
+    };
+}
+
+export function createBankAccountObj(userId: ObjectId, bankAccount_id: ObjectId): IBankAccount {
+    return {
+        _id: bankAccount_id,
+        user_id: userId,
+        name: "Счёт",
+        currency: "BYN",
+        balance: 0,
+        invitingCode: inviteCode(),
+    };
+}
+
+export function inviteCode() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+';
+    let code = '';
+    for (let i = 0; i < 16; i++) {
+        code += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return code;
+}
