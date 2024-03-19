5b3df4630f99ac0bdc0be68db7b042297537823c
vorgnomov <sakolinskayaveronika@mail.ru>
2024-03-19 20:13:50 +0300

    Add pictures for 404 and 500 error pages

---
+++ pages/404.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Button, Container, Text, Title} from '@mantine/core';
+import { useRouter } from 'next/router';
+
+const Error404Page = () => {
+    const router = useRouter();
+
+    const handleBackHome = () => {
+        router.push('/');
+    };
+
+    return (
+        <Container size="sm">
+            <img src='/images/404.png' style={{width: 500, height: 500, marginLeft: 100, marginTop: 100, marginBottom: 50}}/>
+            <Title  order={1} style={{ marginBottom: 10, textAlign: "center"}}>
+                Oops! Something went wrong
+            </Title>
+            <Text  style={{ marginBottom: 20, textAlign: "center"}}>
+                We apologize, but it seems there was an internal server error.
+            </Text>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Button onClick={handleBackHome}>Go back to Home</Button>
+            </div>
+        </Container>
+    );
+};
+
+export default Error404Page;
