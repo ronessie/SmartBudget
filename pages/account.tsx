e9d4afbc742ec76156e9c4717b71306d69a7b984
Yuri Ratkevich <yuri.ratkevich85360@gmail.com>
2024-04-03 01:25:37 +0300

    fix: userBandAccounts

--- pages/account.tsx
+++ pages/account.tsx
@@ -40,7 +40,7 @@ export default function Page(props: { user: IUser, currentBankAccount: ObjectId 
 
     async function findValueForSelect(){
         //const response = await fetch(`/api/userBankAccounts`)
-        const response = await fetch(`/api/userBankAccounts/${JSON.stringify(props.user._id)}`);
+        const response = await fetch(`/api/userBankAccounts/${props.user._id}`);
         if (!response.ok) throw new Error(response.statusText);
     }
 
