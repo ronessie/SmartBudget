a8a592ed8757728b2cf300e1c2a406a68506347d
Veronika <sakolinskayaveronika@mail.ru>
2024-04-11 00:28:49 +0300

    Fix header

--- components/header.tsx
+++ components/header.tsx
@@ -1,42 +1,21 @@
-import { useState } from 'react';
-import {Container, Group, Burger} from '@mantine/core';
+import {Container, Group, Burger, Button} from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import classes from '../styles/header.module.css';
 import {useRouter} from "next/navigation";
 
-const links = [
-    { link: '/main', label: 'Main' },
-    { link: '/', label: 'Index' },
-    { link: '/account', label: 'Account' },
-    { link: '/addingCheck', label: 'AddCheck' },
-];
-
 export default function Header() {
     const [opened, { toggle }] = useDisclosure(false);
-    const [active, setActive] = useState(links[0].link);
     const router = useRouter();
-
-
-    const items = links.map((link) => (
-        <a
-            key={link.label}
-            href={link.link}
-            className={classes.link}
-            data-active={active === link.link || undefined}
-            onClick={(event) => {
-                setActive(link.link);
-            }}
-        >
-            {link.label}
-        </a>
-    ));
 
     return (
         <header className={classes.header}>
             <Container size="md" className={classes.inner}>
                 <img src="/images/small_logo.svg" alt="SmartBudget" style={{paddingTop: 9}} onClick={()=> router.push('/')}/>
                 <Group gap={5} visibleFrom="xs">
-                    {items}
+                    <Button className={classes.button} onClick={()=> router.push('/main')}>Main</Button>
+                    <Button className={classes.button} onClick={()=> router.push('/account')}>Account</Button>
+                    <Button className={classes.button} onClick={()=> router.push('/addingCheck')}>AddingCheck</Button>
+                    <Button className={classes.button} onClick={()=> router.push('/checks')}>Checks</Button>
                 </Group>
                 <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
             </Container>
