9ae5231b5aaa404b14c7ce80ff115c61bf862025
Veronika <sakolinskayaveronika@mail.ru>
2024-04-30 18:04:31 +0300

    Fix footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,30 +1,46 @@
-import {Text, ActionIcon, Group, rem} from '@mantine/core';
-import {IconBrandTwitter, IconBrandYoutube, IconBrandInstagram} from '@tabler/icons-react';
+import { Container, Group, Anchor, Divider } from '@mantine/core'
 import classes from '../styles/footer.module.css';
-import {useRouter} from "next/router";
+import { useTranslation } from 'next-i18next'
+
+const links = [
+    { link: '#', label: 'About' },
+    { link: '#', label: 'Contact' },
+    { link: '#', label: 'Privacy' },
+    { link: '#', label: 'Help' },
+]
 
 export default function Footer() {
-    const router = useRouter();
+    const { t } = useTranslation()
+
+    const items = links.map((link) => (
+        <Anchor<'a'>
+            c='dimmed'
+            key={link.label}
+            href={link.link}
+            onClick={(event) => event.preventDefault()}
+            size='sm'
+        >
+            {link.label}
+        </Anchor>
+    ))
 
     return (
-        <footer className={classes.footer}>
-            <img src="/images/whiteLogo.svg" alt="SmartBudget" style={{paddingTop: 9}}
-                 onClick={() => router.push('/')}/>
-            <Text c="dimmed" size="sm">
-                © 2024 mantine.dev. All rights reserved.
-            </Text>
-
-            <Group gap={0} className={classes.social} justify="flex-end" wrap="nowrap">
-                <ActionIcon size="lg" color="gray" variant="subtle">
-                    <IconBrandTwitter style={{width: rem(18), height: rem(18)}} stroke={1.5}/>
-                </ActionIcon>
-                <ActionIcon size="lg" color="gray" variant="subtle">
-                    <IconBrandYoutube style={{width: rem(18), height: rem(18)}} stroke={1.5}/>
-                </ActionIcon>
-                <ActionIcon size="lg" color="gray" variant="subtle">
-                    <IconBrandInstagram style={{width: rem(18), height: rem(18)}} stroke={1.5}/>
-                </ActionIcon>
-            </Group>
+        <footer
+            style={{
+                position: 'absolute',
+                bottom: '0',
+                left: '0',
+                width: '100%',
+                textAlign: 'center',
+                height: '25px',
+                marginBottom: '2rem',
+            }}
+        >
+            <Divider mb={'sm'}/>
+            <Container className={classes.inner}>
+                <h1>© SmartBudget. All Rights Reserved. 2024</h1>
+                <Group>{items}</Group>
+            </Container>
         </footer>
-    );
+    )
 }
