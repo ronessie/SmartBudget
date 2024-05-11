545461d81434c4e2a27c240a7f7693e14e6955e3
Veronika <sakolinskayaveronika@mail.ru>
2024-05-11 21:53:41 +0300

    fix 404, 500 pages localization

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Button, Container, Image, Text, Title} from '@mantine/core';
 import {useRouter} from 'next/router';
+import {useTranslation} from "next-i18next";
+import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 
 const Error404Page = () => {
     const router = useRouter();
+    const { t } = useTranslation('common');
 
     const handleBackHome = () => {
         router.push('/');
@@ -13,17 +16,21 @@ const Error404Page = () => {
         <Container size="sm">
             <Image src='/images/404.png' alt="Image 404"
                    style={{width: 500, height: 500, marginLeft: 100, marginTop: 100, marginBottom: 50}}/>
-            <Title order={1} style={{marginBottom: 10, textAlign: "center"}}>
-                Oops! Something went wrong
-            </Title>
-            <Text style={{marginBottom: 20, textAlign: "center"}}>
-                We apologize, but it seems there was an internal server error.
-            </Text>
+            <Title order={1} style={{marginBottom: 10, textAlign: "center"}}>{t('404.title')}</Title>
+            <Text style={{marginBottom: 20, textAlign: "center"}}>{t('404.text')}</Text>
             <div style={{display: 'flex', justifyContent: 'center'}}>
-                <Button onClick={handleBackHome}>Go back to Home</Button>
+                <Button onClick={handleBackHome}>{t('404.button')}</Button>
             </div>
         </Container>
     );
 };
 
 export default Error404Page;
+
+export const getServerSideProps = async (ctx: any) => {
+    return {
+        props: {
+            ...(await serverSideTranslations(ctx.locale, ['common']))
+        }
+    }
+};
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {Button, Container, Image, Text, Title} from '@mantine/core';
 import {useRouter} from 'next/router';
+import {useTranslation} from "next-i18next";
+import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 
 const Error500Page = () => {
     const router = useRouter();
+    const { t } = useTranslation('common');
 
     const handleBackHome = () => {
         router.push('/');
@@ -13,17 +16,21 @@ const Error500Page = () => {
         <Container size="sm">
             <Image src='/images/500.png' alt="Image 500"
                    style={{width: 500, height: 500, marginLeft: 100, marginTop: 100, marginBottom: 50}}/>
-            <Title order={1} style={{marginBottom: 10, textAlign: "center"}}>
-                Internal Server Error
-            </Title>
-            <Text style={{marginBottom: 20, textAlign: "center"}}>
-                Sorry, something went wrong on our end. Please try again later.
-            </Text>
+            <Title order={1} style={{marginBottom: 10, textAlign: "center"}}>{t('500.title')}</Title>
+            <Text style={{marginBottom: 20, textAlign: "center"}}>{t('500.text')}</Text>
             <div style={{display: 'flex', justifyContent: 'center'}}>
-                <Button onClick={handleBackHome}>Go back to Home</Button>
+                <Button onClick={handleBackHome}>{t('500.button')}</Button>
             </div>
         </Container>
     );
 };
 
 export default Error500Page;
+
+export const getServerSideProps = async (ctx: any) => {
+    return {
+        props: {
+            ...(await serverSideTranslations(ctx.locale, ['common']))
+        }
+    }
+};
