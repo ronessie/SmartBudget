0bd711e343a8b9b5193f24908f6b4a1caaf54af9
Veronika <sakolinskayaveronika@mail.ru>
2024-07-19 21:28:42 +0300

    Update locale

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,10 @@
+const path = require('path')
+
 module.exports = {
     i18n: {
         defaultLocale: 'ru',
+        localeDetection: false,
         locales: ['ru', 'en'],
+        localePath: path.resolve('./public/locales'),
     },
 }
