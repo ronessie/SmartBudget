9f706ba65606d518d8d64a91f73497dccac5c267
Veronika <sakolinskayaveronika@mail.ru>
2024-07-26 20:37:02 +0300

    add firebaseConfig and npm

---
+++ firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAicdD45Q4uWzS3_oj9Vasaw5_1pdRk5-I",
+    authDomain: "smartbudget-878b0.firebaseapp.com",
+    projectId: "smartbudget-878b0",
+    storageBucket: "smartbudget-878b0.appspot.com",
+    messagingSenderId: "764783757546",
+    appId: "1:764783757546:web:64931c70385bb3c2faef93",
+    measurementId: "G-96TZTKVQ4V"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
