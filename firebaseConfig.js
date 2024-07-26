5d452733196190a2e0dfa858d216808588f76ffa
Veronika <sakolinskayaveronika@mail.ru>
2024-07-26 22:01:15 +0300

    Update firebaseConfig and env files

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import {getStorage} from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAicdD45Q4uWzS3_oj9Vasaw5_1pdRk5-I",
-    authDomain: "smartbudget-878b0.firebaseapp.com",
-    projectId: "smartbudget-878b0",
-    storageBucket: "smartbudget-878b0.appspot.com",
-    messagingSenderId: "764783757546",
-    appId: "1:764783757546:web:64931c70385bb3c2faef93",
-    measurementId: "G-96TZTKVQ4V"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
